72b12251cb47c49eec920bc187ebfe9aced581da
varun <varunraj82786@gmail.com>
2021-12-12 10:38:56 +0530

    Login Updated

--- login.php
+++ login.php
@@ -22,23 +22,61 @@
                     <a href="#">Login</a>
                 </h2>
 
-                <form>
+                <form action="" method="POST" name="login">
                     <div class="inputBx">
                         <span>Username</span>
-                        <input name="email" type="email" />
+                        <input name="username" type="text" />
                     </div>
                     <div class="inputBx">
                         <span>Password</span>
                         <input name="password" type="password" />
                     </div>
                     <div class="inputBx">
-                        <input type="submit" value="sign in" />
+                        <input type="submit" name="submit" value="sign in" />
                     </div>
-
                 </form>
             </div>
         </div>
     </section>
+
 </body>
 
 </html>
+<?php 
+if(isset($_POST['submit']))
+{
+  if(!empty($_POST['username']) && !empty($_POST['password']))
+	{
+	$user=$_POST['username'];  
+        $pass=$_POST['password']; 
+	$con=mysql_connect('localhost','root','') or die(mysql_error());  
+        mysql_select_db('user') or die("cannot select DB");  
+        $query=mysql_query("SELECT * FROM login WHERE user_id='".$user."' AND password ='".$pass."'");  
+        $numrows=mysql_num_rows($query); 
+	if($numrows!=0) 
+            { 
+                while($row=mysql_fetch_assoc($query))  
+                {  
+                    $dbusername=$row['user_id'];  
+                    $dbpassword=$row['password'];  
+                } 
+                if($user == $dbusername && $pass == $dbpassword)  
+                    {  
+                        session_start();  
+                        $_SESSION['sess_user']=$user;  
+                        header("Location: dashboard.php");  
+                    } 
+                else
+                {
+                 echo'<script>';
+                 echo ' alert("Invalid username or password!")';
+                 echo'</script>' ;
+                }
+            }	  	
+	}
+	else
+	 {  
+        	echo'alert("All fields are required!")';  
+         }  
+} 
+?>  
