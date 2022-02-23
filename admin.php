60c515495fe2823875cbabe57f29c85b1374ba6b
kushal gohil <2002kushalgohil@gmail.com>
2022-02-23 10:36:13 +0530

    Finished Admin UI

--- admin.php
+++ admin.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,10 +8,15 @@
     <link rel="stylesheet" href="./Assets/Style/style.css?v=<?php echo time(); ?>">
     <title>Admin @ Portal</title>
 </head>
+
 <body style="display: flex;justify-content:center;background-color:beige;">
-    <h1 >This portal is under construction ... ! </h1>
-    <div>
-            <a href="dashboard.php"><button>Go to Dashboard !</button></a>
+    <section class="section">
+        <div>
+            <h1 style="margin-bottom: 20px;">Admin Section is under construction ... ! </h1>
+            <a href="dashboard.php"><button class="btn">Go to Dashboard !</button></a>
         </div>
+
+    </section>
 </body>
+
 </html>
